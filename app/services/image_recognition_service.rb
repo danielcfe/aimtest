4b965a1e3ee886c41263caddc513a35282ff2ae5
daniel castillo <danielcastillo@MacBook-Pro-de-daniel.local>
2017-04-25 22:02:52 -0300

    fix response var name

--- app/services/image_recognition_service.rb
+++ app/services/image_recognition_service.rb
@@ -13,8 +13,8 @@ class ImageRecognitionService
   private
   def connect
     resource = RestClient::Resource.new( uri_email(@email), ENV['WS2_USER'], ENV['WS2_PASS'] )
-    resource.post(payload)
-    (resource.code == 200)
+    res = resource.post(payload)
+    (res.code == 200)
   end
 
   def uri_email(email)
